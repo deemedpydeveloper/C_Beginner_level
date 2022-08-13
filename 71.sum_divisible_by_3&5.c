32f36592021b4d42b2189e21146f7a65af89dd16
tejendra <tejendrayerra2000.4@gmail.com>
2022-08-13 22:48:39 +0530

    Add files via upload

---
+++ 71.sum_divisible_by_3&5.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+int main()
+{
+    int num, i, sum =0;
+    printf("Enter the number: ");
+    scanf("%d", &num);
+    for (i = 1; i <= num; i ++)
+    {
+        if ((i % 3 == 0) &&(i % 5 == 0))
+        {
+            sum += i;
+        }
+    }
+    printf("The sum of numbers is: %d\n", sum);
+    return 0;
+}
